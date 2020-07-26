7b19a077e6436cab85ef70998a777c351922ceba
admin <example@gmail.com>
2020-07-26 20:45:47 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hellow');	
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof (answer));
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Как вам лет?', '');
+
+console.log(typeof (answers));
